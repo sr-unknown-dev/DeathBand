58c8d109977aafbce0bc7679acb5e9c23e14630d
Ghost-zzz <idk811959@gmail.com>
2024-10-28 15:16:32 -0600

    Asd

--- src/Ghost/Loader.php
+++ src/Ghost/Loader.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Copyright (c) 2024 Ghost. All rights reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 namespace Ghost;
 
 use Ghost\Manager\LivesManager;
